c3bdabb3ddff8371d041cae2b691de66e2f28c6f
sneha <sangaonkarsneha@gmail.com>
2025-02-04 20:54:25 +0530

    ProducingJson_05

--- src/main/java/com/lcwd/mvc/SpringMvcProject/controllers/StudentController.java
+++ src/main/java/com/lcwd/mvc/SpringMvcProject/controllers/StudentController.java
@@ -3,6 +3,8 @@ package com.lcwd.mvc.SpringMvcProject.controllers;
 import com.lcwd.mvc.SpringMvcProject.models.Student;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,31 +14,17 @@ public class StudentController {
 
 
     @PostMapping("/create")
-    // here @RequestBody will help us to get the data that has passed in post request body will get converted in Map(json serializing)
-    // json -> java object ==>(deserialization)
-//    java object -> json ==> (Serialization)
-    // deserialization is automatic(jackson library)
+    public Map<String, Object> createStudent(@RequestBody List<Student> students) {
 
-    //not good way
-//    public String createStudent(@RequestBody Map<String, Object> data) {
+        System.out.println("Student list size:" +students.size());
+        System.out.println(students);
 
-//    correct way: Custom type(Create custom model student)
-        public Student createStudent(@RequestBody Student student) {
-        // we have to create student
-        // we are receiving data in json format
-//        System.out.println(data);
+        Map<String, Object> data = new HashMap<>();
+        data.put("content", students);
+        data.put("error", "No error found");
+        data.put("current data", new Date());
+//        data.put("System Information", System.getProperties());
 
-//        Object id= data.get("id");
-//        Object title= data.get("title");
-//        System.out.println("Id: " +id);
-//        System.out.println("Title: " +title);
-
-        System.out.println(student);
-
-//        if there was List of students:
-//        public String createStudent(@RequestBody List<Student> students) {
-
-//        return "created";
-        return student;
+        return data;
     }
 }
