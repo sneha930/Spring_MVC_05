9ded7171fb0e09715489706ea9ec5cd76f53882f
sneha <sangaonkarsneha@gmail.com>
2025-02-05 18:38:16 +0530

    URIPathVariable@PathVariable_05

--- src/main/java/com/lcwd/mvc/SpringMvcProject/controllers/ProductController.java
+++ src/main/java/com/lcwd/mvc/SpringMvcProject/controllers/ProductController.java
@@ -1,8 +1,6 @@
 package com.lcwd.mvc.SpringMvcProject.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -13,5 +11,18 @@ public class ProductController {
         System.out.println("Product Id" + id);
         System.out.println("Product rating" +rating);
         return "This is testing product url";
+    }
+
+    // here we are expecting dynamic value here by pacing curly brackets ie. {productId}
+    @RequestMapping("/checkProduct/{productId}/{productName}/{productRating}")
+    public String checkProduct(
+            @PathVariable("productId") int id,
+            @PathVariable String productName,
+            @PathVariable int productRating
+    ) {
+        System.out.println("Product Name" + productName);
+        System.out.println("Product Id" + id);
+        System.out.println("Product rating" + productRating);
+        return "this is checking the concept of path variable";
     }
 }
